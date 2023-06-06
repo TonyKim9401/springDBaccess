0acd22cf3495a1982791dcd38d6df039a42d3489
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-06-07 00:17:48 +0900

    JUNE.05.2023 - MyBatis 적용 기본
    @Mapper 를 붙여 '도메인 이름 + Mapper' 로 인터페이스를 만들고 메소드를 작성한다.
    
    resources 디렉토리에 인터페이스 경로와 같은 xml파일을 생성 후 mybatis 문법에 맞추서 쿼리문을 작성한다.
    
    #resources/mapper를 포함한 그 하위 폴더에 있는 XML을 매핑 파일로 인식한다.
    #이 경우 파일 이름은 자유롭게 설정해도 된다.
    #mybatis.mapper-locations=classpath:mapper/**/*.xml

---
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,26 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    //파라미터가 2개 이상 넘어갈 경우에는 @Param을 붙여준다.
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+
+
+
+}
