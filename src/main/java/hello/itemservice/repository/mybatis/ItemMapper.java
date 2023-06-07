fe3188c707f6c11faa4a7160bfce981ea81abd8f
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-06-07 21:08:04 +0900

    JUNE.07.2023 - MyBatis 분석
    실행시 MyBatis에 의해 ItemMapper 인터페이스가 Proxy 객체로 동적으로 생성 되어 Spring Bean에 등록되기 때문에 Proxy구현체로서 사용 가능하다.
    
    log.info("itemMapper class={}", itemMapper.getClass());
    로 확인시 itemMapper class=class com.sun.proxy.$Proxy66 가 생성된 것을 확인 할 수 있다.
    
    Mapper 구현체 사용으로 스프링에서 편리하게 통합해서 사용할 수 있다.
    
    DataAccessException같은 스프링 예외 추상화도 함께 저용된다.

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -5,6 +5,7 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,12 @@ public interface ItemMapper {
 
     //파라미터가 2개 이상 넘어갈 경우에는 @Param을 붙여준다.
     void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    /**
+        @Select("select id, item_name, price, quantity from item where id = #{id}")
+     * 인터페이스에 위와 같이 직접 애노테이션을 붙여서 쿼리문을 작성하는 것도 가능하다.
+     * 하지만, xml와 겹치면 충돌오류가 발생하니 둘 중 하나만 선택하여야 한다.
+     */
 
     Optional<Item> findById(Long id);
 
